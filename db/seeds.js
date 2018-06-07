6c473bf7884248ecaf82c48ae784e829821f9d20
plaifang7 <plaifang7@gmail.com>
2018-06-07 16:06:03 -0400

    added dog to user and updated database

--- db/seeds.js
+++ db/seeds.js
@@ -30,42 +30,45 @@ User.remove()
       name: 'King',
       breed: 'Pitbull Mix',
       age: 1,
-      description: 'King is a very energetic dog who loves to play with other dogs, both big and small. He is mixed with Pitbull and Jack Russell Terrier',
-      image: 'String'
+      description: 'King is a very energetic dog who loves to play with other dogs, both big and small. He is mixed with Pitbull and Jack Russell Terrier, which illustrates just the ball of energy he is! His favorite toys are his ball, kong and chewing ring. He loves to play fetch and catches like a champ. ',
+      image: 'https://imgur.com/KePgseE'
     })
 
     const dog3 = new Dog({
-      name: 'String',
-      breed: 'String',
+      name: 'Princess',
+      breed: 'Pitbull',
       age: 3,
-      description: 'String',
-      image: 'String'
+      description: 'Princess is a fun-loving Pitty who loves to play with her brother King. Although they differ in size, they play as if they are the same size. Her favorite toys are her ball, her rope and her cone.',
+      image: 'https://i.pinimg.com/originals/8f/9a/0d/8f9a0d9e75359af7b95b200d21fdc0e6.jpg'
     })
 
     const dog4 = new Dog({
-      name: 'String',
-      breed: 'String',
+      name: 'Jimbo',
+      breed: 'Sharpei',
       age: 1,
-      description: 'String',
-      image: 'String'
+      description: 'Jimbo is a well-trained, very playful Sharpei. He plays well with other big dogs. As far as small dogs, he can get a little rough, but is trained to stop when he gets out of hand. His favorite toy is his ball.',
+      image: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/shar-pei-playing-in-a-spare-tire-jeanne-white.jpg'
     })
 
     const user1 = new User({
       name: 'Peter Whiskers',
       image: 'http://www.digitaljournal.com/img/8/7/8/i/7/9/0/o/wesPress3.jpg',
       favPark: 'Piedmont Dog Park',
+      dog: [dog1]
     })
 
     const user2 = new User({
       name: 'Patrick Lai-Fang',
       image: 'https://imgur.com/0nXkegq',
-      favPark: 'Glenlake Dog Park'
+      favPark: 'Glenlake Dog Park',
+      dog: [dog2, dog3]
     })
 
     const user3 = new User({
       name: 'Nigel Ratburn',
       image: 'https://imgur.com/0nXkegq',
-      favPark: 'Oakhurst Dog Park'
+      favPark: 'Oakhurst Dog Park',
+      dog: [dog4]
     })
 
     const users = [user1, user2, user3]

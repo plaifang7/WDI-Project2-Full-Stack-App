9733b147dc257ad878d473972a031963a2c3cf14
plaifang7 <plaifang7@gmail.com>
2018-06-07 11:16:18 -0400

    doginder initiated'

--- app.js
+++ app.js
@@ -30,6 +30,21 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development

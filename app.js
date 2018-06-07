2ff10ae3392cba25c3b7229016f2c468afcc6ce1
plaifang7 <plaifang7@gmail.com>
2018-06-06 20:52:40 -0400

    added mongoDB

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI); 
 
 var app = express();
 

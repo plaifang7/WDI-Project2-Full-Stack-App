24c765bb7af0728e82cd58da4e129abb8561952b
plaifang7 <plaifang7@gmail.com>
2018-06-08 10:17:41 -0400

    having a hard time figure out my routes and views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dinder' });
+  res.render('user/index', { title: 'Dinder' });
 });
 
 module.exports = router;
